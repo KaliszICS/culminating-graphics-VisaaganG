f1b13a8ffd33ff5aff737108f4413cac351b8dcb
VisaaganG <343505582@tdsb.ca>
2026-06-03 15:38:57 +0000

    Second Commit

--- Wordle.java
+++ Wordle.java
@@ -4,7 +4,10 @@ import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.geometry.Pos;
-
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 
 public class Wordle extends Application {
@@ -48,10 +51,81 @@ public class Wordle extends Application {
 
         //creates the window to play in and adds the "board" onto it
         Scene scene = new Scene(board, 600, 700);
+
+        //
+        TextField guessField = new TextField();
+        board.add(guessField, 0, 7, 5, 1);
+
+        Button guessButton = new Button("Enter");
+        board.add(guessButton, 0, 8, 5, 1);
+
+        Label message = new Label("Enter a 5-letter word");
+        board.add(message, 0, 9, 5, 1);
+
+
+        guessButton.setOnAction(e -> {
+        String guess;
+        guess = guessField.getText().toUpperCase().trim();
+
+            if (guess.length() != 5) {
+                message.setText("Word must be 5 Letters!");
+                return;
+            }
+
+            if (currentRow >= 6) {
+                return;
+            }
+            //Validating input, matching guess with answer, and providing feedback
+            for (int col = 0; col < 5; col++) {
+
+                char guessLetter = guess.charAt(col);
+                char answerLetter = randomWord.charAt(col);
+
+                cells[currentRow][col].setText(
+                    String.valueOf(guessLetter)
+                );
+                //Green
+                if (guessLetter == answerLetter) {
+                    cells[currentRow][col].setStyle(
+                        "-fx-background-color: green;" +
+                        "-fx-text-fill: white;" +
+                        "-fx-border-color: black;" +
+                        "-fx-font-size: 24px;" +
+                        "-fx-font-weight: bold;"
+                    );
+                }
+                //Yellow
+                else if (randomWord.contains(guessLetter))) {
+
+                    cells[currentRow][col].setStyle(
+                        "-fx-background-color: gold;" +
+                        "-fx-text-fill: black;" +
+                        "-fx-border-color: black;" +
+                        "-fx-font-size: 24px;" +
+                        "-fx-font-weight: bold;"
+                    );
+                }
+
+                //Gray
+                else { 
+                    cells[currentRow][col].setStyle(
+                        "-fx-background-color: lightgray;" +
+                        "-fx-text-fill: black;" +
+                        "-fx-border-color: black;" +
+                        "-fx-font-size: 24px;" +
+                        "-fx-font-weight: bold;"
+                    );
+                }
+            }
         
-        //makes it visible
+
+
+        //makes it all visible
         stage.setScene(scene);
         stage.show();
+        
+        }
+    );
     }
     public static void main(String[] args) {
         launch();
